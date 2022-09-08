dd4a6a36cb5207aabb8283bcb22ded9540625434
Edgaras Vaninas <e.vaninas@gmail.com>
2022-09-08 22:23:52 +0300

    Completed module

--- vehicle.go
+++ vehicle.go
@@ -115,7 +115,7 @@ func generateRating() {
 		for _, msg := range v.Feedback {
 			if text := strings.Split(msg, " "); len(text) >= 5 {
 				vehRating = 5.0
-				vehResult++
+				vehResult = feedbackTotal++
 			}
 		}
 	}
